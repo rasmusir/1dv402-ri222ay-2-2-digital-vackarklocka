8a8fcd8c7a2731ab62365e15b55823a695487943
rasmusir <israelssonrasmusraz@gmail.com>
2014-10-16 11:01:57 +0200

    +Adding AlarmClock.cs

---
+++ 1dv402-ri222ay-2-2-digital-vackarklocka/AlarmClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv402_ri222ay_2_2_digital_vackarklocka
+{
+    class AlarmClock
+    {
+    }
+}
